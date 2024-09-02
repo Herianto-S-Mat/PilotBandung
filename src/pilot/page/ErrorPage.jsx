218419d2901f971f2e4cb67470be628586e3a22d
MadeBy-Herianto <herianto9671@gmail.com>
2024-09-03 00:56:15 +0700

    error boundary

--- src/pilot/page/ErrorPage.jsx
+++ src/pilot/page/ErrorPage.jsx
@@ -1,14 +1,22 @@
 import { Container} from 'react-bootstrap'
-import BrandLogo from '/main-logo.svg'
+import BrandLogo from '/Untitled.png'
 export const ErrorPage = () => {
   return (
       <Container fluid className="d-flex justify-content-center align-items-center vh-100 vw-100">
         <div className="text-center p-5">
-          <img src={BrandLogo} className='w-75 mb-5 bg-danger rounded '/>
-          <h3 className='text-danger'>Something Wrong in this page</h3>
-          <p>
+          <h1 className='pb-3'>PILOT BANDUNG</h1>
+          <img src={BrandLogo} className='w-75 mb-5 bg-danger rounded '/>          
+          
+            <p className='text-danger'>
+              Maaf jika mengalami gangguan!!!
+              <br />
+              <br />
+              Halaman ini sedang dalam proses pengembangan sehingga belum berfungsi optimal
+            </p>
+          
+          {/* <p>
             <a href='/' className='btn bg-dark text-warning m-2'>Go to home</a>
-          </p>
+          </p> */}
         </div>
       </Container>
   )
