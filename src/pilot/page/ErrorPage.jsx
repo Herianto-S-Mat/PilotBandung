29afeb3b368f8d58b5beaa73382ab70d470656cb
MadeBy-Herianto <herianto9671@gmail.com>
2024-09-03 01:03:47 +0700

    eror boundary style image

--- src/pilot/page/ErrorPage.jsx
+++ src/pilot/page/ErrorPage.jsx
@@ -5,14 +5,14 @@ export const ErrorPage = () => {
       <Container fluid className="d-flex justify-content-center align-items-center vh-100 vw-100">
         <div className="text-center p-5">
           <h1 className='pb-3'>PILOT BANDUNG</h1>
-          <img src={BrandLogo} className='w-75 mb-5 bg-danger rounded '/>          
+          <img src={BrandLogo} className='mb-5 bg-danger rounded' style={{width:'300px', maxWidth:'50%'}}/>          
           
-            <p className='text-danger'>
-              Maaf jika mengalami gangguan!!!
-              <br />
-              <br />
-              Halaman ini sedang dalam proses pengembangan sehingga belum berfungsi optimal
-            </p>
+          <p className='text-danger'>
+            Maaf jika mengalami gangguan!!!
+            <br />
+            <br />
+            Halaman ini sedang dalam proses pengembangan sehingga belum berfungsi optimal
+          </p>
           
           {/* <p>
             <a href='/' className='btn bg-dark text-warning m-2'>Go to home</a>
