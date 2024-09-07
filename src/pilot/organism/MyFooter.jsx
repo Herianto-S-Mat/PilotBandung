498a26fea2a4e6623f1c50d30ecf87bc7b1d0809
MadeBy-Herianto <herianto9671@gmail.com>
2024-09-07 21:43:12 +0700

    galeri Mansory

--- src/pilot/organism/MyFooter.jsx
+++ src/pilot/organism/MyFooter.jsx
@@ -24,7 +24,7 @@ export const MyFooter = () => {
 	}, []);
 
   return (
-<Container fluid className="bg-dark bg-gradient text-warning mt-5" style={{borderRadius:'1em 1em 0 0'}}>
+<Container fluid className="bg-dark bg-gradient text-warning" style={{borderRadius:'1em 1em 0 0'}}>
     <Row id='kontak-kami' className='pt-5 p-3 w-100'>
 		{data.filter((info)=> info.entity_name!='Copyright' ).map((data_info, index) => (
 			<Col md={6} xxl={3} key={index}  style={{ display: data_info.entity_name.includes('google') ? 'none' : '' }}>
