37f2b4ef8f359ae1c3760046c29263380959848c
MadeBy-Herianto <herianto9671@gmail.com>
2024-09-09 00:32:05 +0700

    revisi footer

--- src/pilot/organism/CorouselPictureMansory.jsx
+++ src/pilot/organism/CorouselPictureMansory.jsx
@@ -2,80 +2,8 @@ import './organism.css'
 import failed_img from '/failed-img.jpg';
 import { pictures } from '../../data/data_backend';
 import { useEffect, useState } from 'react';
-
-
-// export const CorouselPicture = () => {
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     pictures
-//       .then((picture) => {
-//         const new_data = picture.reduce((acc, item) => {
-//           if (!acc[item.tipe_gambar]) {
-//             acc[item.tipe_gambar] = [];
-//           }
-//           acc[item.tipe_gambar].push(item);
-//           return acc;
-//         }, {})
-//         setData(new_data);
-//       })
-//       .catch((error) => {
-//         console.error('Failed to fetch layanan data:', error);
-//       });
-//   }, []);  
-  
-//   return (
-//     <>
-//       {
-//         Object.keys(data)
-//           .filter(type => !['banner', 'logo', 'About Us'].includes(type))
-//           .map((type, i) => (
-//             <div className='mb-5' key={i}>
-//               <h5 className='mx-5 border border-dark bg-warning-subtle rounded p-2'> {type} </h5>
-//             </div>
-//           ))
-//       }
-//     </>
-//   );
-  
-// }
-
-
-// const ItemCorousel = ({data}) => {
-//   return (
-//     <>
-//       <img 
-//         alt="gambar"
-//         className='img-fluid rounded border border-dark'
-//         src={data.url_gambar} 
-//         onError={(e) => e.target.src = failed_img}
-//       />
-//       {(!['-', '', 'hide','Hide', '_'].includes(data.nama_gambar)) && (
-//         <>
-//           <br />
-//           <div className='my-2 bg-dark-subtle rounded'>
-//             <b style={{textWrap:'wrap'}}>{data.nama_gambar}</b>
-//           </div>
-//         </>
-
-//       )}
-//     </>
-//   )
-// }
-
-
-import React from 'react';
 import Masonry from 'react-masonry-css';
 import { Card, Container } from 'react-bootstrap';
-
-const imageData = [
-  // Tambahkan data gambar di sini, misalnya:
-  { id: 1, src: 'https://via.placeholder.com/300x400', title: 'Gambar 1' },
-  { id: 2, src: 'https://via.placeholder.com/300x300', title: 'Gambar 2' },
-  { id: 3, src: 'https://via.placeholder.com/300x500', title: 'Gambar 3' },
-  { id: 4, src: 'https://via.placeholder.com/300x350', title: 'Gambar 4' },
-  // dan seterusnya
-];
 
 export const GalleryMasonry = () => {
   const breakpointColumnsObj = {
--- src/pilot/organism/MyFooter.jsx
+++ src/pilot/organism/MyFooter.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Container, Row, Col, Alert, Button } from 'react-bootstrap';
+import { Container, Row, Col, Alert, Button, Tooltip, OverlayTrigger, ButtonToolbar, ButtonGroup } from 'react-bootstrap';
 import { info } from '../../data/data_backend';
 import { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
-import CopyText from './CopyText';
 import './organism.css'
 import { ContactButton, WaButton } from './MyButton';
 import go_to_page from '/goto_page.svg'
@@ -23,58 +22,119 @@ export const MyFooter = () => {
 	);
 	}, []);
 
+	const [copied, setCopied] = useState(false);
+	const handleCopy = async(text) => {
+		if(!copied){
+			await navigator.clipboard.writeText(text);
+			setCopied(true);
+		
+			setTimeout(() => {
+				setCopied(false);
+			  }, 2000);
+			// Kembalikan teks "Salin" setelah 2 detik
+		}
+	};
+
+  const [activeOverlay, setActiveOverlay] = useState(null);
+
+  // Fungsi untuk menampilkan overlay
+  const handleToggle = (id) => {
+    // Jika overlay yang diklik sudah aktif, maka nonaktifkan
+    if (activeOverlay === id) {
+      setActiveOverlay(null);
+    } else {
+      // Jika ada overlay lain yang aktif, nonaktifkan dan aktifkan yang baru
+      setActiveOverlay(id);
+    }
+  };
+  
   return (
-<Container fluid className="bg-dark bg-gradient text-warning mt-5" style={{borderRadius:'1em 1em 0 0'}}>
-    <Row id='kontak-kami' className='pt-5 p-3 w-100'>
+<Container fluid className="bg-dark mt-5" style={{borderRadius:'1em 1em 0 0'}}>
+    <div id='kontak-kami' className='d-flex justify-content-center flex-wrap py-3 mx-auto border-2 border-bottom border-light'>
 		{data.filter((info)=> info.entity_name!='Copyright' ).map((data_info, index) => (
-			<Col md={6} xxl={3} key={index}  style={{ display: data_info.entity_name.includes('google') ? 'none' : '' }}>
-				<p className='mb-1 fs-5'>
-					{data_info.entity_name}
-				</p>				
-				<Alert variant='warning' className='text-wrap d-flex align-items-center'>
-					<span className='me-auto'>
-						  {data_info.entity_name !== "Phone" && !data_info.entity_name.includes("kantor")
-							? `Visit our ${data_info.entity_name}`
-							: data_info.entity_name.includes("kantor")
-							? data_info.detail
-							: `Contact us via ${data_info.detail}`}
-					</span>
-					{['https://'].includes(data_info.detail.slice(0,8)) && (
-						<Button variant='none' className='ms-auto' href={data_info.detail}>
-						<img src={go_to_page} alt="link" />
-						</Button>)
-					}
-					{data_info.entity_name=='Phone'
-						&& createPortal(
-							<div className="text-center">
-								<h5>
-									Segera jadwalkan keperluan anda melalui kontak kami !!!
-								</h5>
-								<h1 className=" bg-light rounded py-3">{data_info.detail}</h1>
-								<WaButton message={`Halo!!! Saya mau menanyakan layanan Pilot Bandung, saya ada jadwal`}> 
-									Hubungi Kami 
-								</WaButton>
-								<ContactButton> 
-									Hubungi Kami 
-								</ContactButton>
-							</div>
-							,
-							document.getElementById('operation-hours-contact')
-						) 
-					}
-				</Alert>
-			</Col>
+			<div key={index} className='text-light m-3'>
+				    <OverlayTrigger
+						show={activeOverlay === index} 
+						trigger="click"
+						placement="top"
+						rootClose 
+						overlay={
+							<Tooltip id="tooltip" style={{transform:'translateY(-5em)'}}>
+								<p className='bg-dark p-2 bg-opacity-5 rounded'>{data_info.detail}</p>
+								<div className='mb-3 w-full d-flex justify-content-center'>
+									<ButtonToolbar className="position-relative">
+										<ButtonGroup size="sm">
+										{['Instagram','WhatsApp'].includes(data_info.entity_name)&& <Button variant="none" className="text-warning border border-warning" href={data_info.detail}>lihat</Button>}
+										{['Phone'].includes(data_info.entity_name) && <Button variant="none" className="text-warning border border-warning" href={`tel:${data_info.detail}`}>panggil</Button>}
+										<Button variant="none" className="text-warning border border-warning"  onClick={()=>handleCopy(data_info.detail)}>Salin</Button>
+										 {copied && 
+										 	<div 
+												className="copied-message position-absolute text-center bg-dark border text-warning rounded shadow-sm"
+												style={{
+													top:'0',
+													left:'0',
+													width:'100%',
+													transform:'translateY(-110%)',
+													opacity:'50%'
+
+												}}
+											>
+											 Tersalin!
+										   </div>
+										}
+										</ButtonGroup>
+									</ButtonToolbar>
+								</div>
+						  	</Tooltip>
+						}
+					>
+					{/* <img src={go_to_page} alt="link" onClick={() => handleToggle(index)} /> */}
+					{/* <i class="fa-brands fa-instagram"></i> */}
+					<i 
+						className={`${
+							(data_info.entity_name =="Instagram" && "fa-brands fa-instagram") ||
+							(data_info.entity_name =='Phone' && "fa-solid fa-phone") ||
+							(data_info.entity_name =='WhatsApp' && "fa-brands fa-whatsapp") ||
+							(data_info.entity_name =='Alamat kantor' && "fa-solid fa-location-dot")
+						} fa-2xl` }
+						onClick={() => handleToggle(index)}
+					/>
+				</OverlayTrigger>			
+				{data_info.entity_name=='Phone'
+					&& createPortal(
+						<div className="text-center">
+							<h5>
+								Segera jadwalkan keperluan anda melalui kontak kami !!!
+							</h5>
+							<h1 className=" bg-light rounded py-3">{data_info.detail}</h1>
+							<WaButton message={`Halo!!! Saya mau menanyakan layanan Pilot Bandung, saya ada jadwal`}> 
+								Hubungi Kami 
+							</WaButton>
+							<ContactButton> 
+								Hubungi Kami 
+							</ContactButton>
+						</div>
+						,
+						document.getElementById('operation-hours-contact')
+					) 
+				}
+			</div>
 		))}
-    </Row>
+    </div>
 	<Row className='p-3'>
 	{data.filter((info)=> info.entity_name=='Copyright').map((data_info, index) => (
-		<Alert variant='light' className='text-center' key={index}>
+		<div className='text-center text-warning' key={index}>
 			<b>{data_info.entity_name} @ </b>
 			<a>{data_info.detail}</a>
-		</Alert>
+		</div>
 	))}
 	</Row>
 </Container>
   );
 };
 
+const tooltip = (
+	<Tooltip id="tooltip">
+	  <strong>Holy guacamole!</strong> Check this info.
+	</Tooltip>
+  );
